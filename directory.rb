553b53f10ce1b2f19f0509188f8e791fa01627fc
Kornelia <ez.kornelia@gmail.com>
2016-10-22 19:22:53 +0100

    Adding the directory of villains

---
+++ directory.rb
@@ -0,0 +1,25 @@
+def line
+  puts "-" * 33
+end
+
+def space
+  puts ""
+end
+
+space
+puts "The students of Villains Academy"
+line
+space
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+line
+space
