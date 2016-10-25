a15e04e1c0ef73cf7145026c0eef39dac1b2569e
Kornelia <ez.kornelia@gmail.com>
2016-10-25 20:20:14 +0100

    Task 3 is done

--- directory.rb
+++ directory.rb
@@ -1,16 +1,14 @@
 #no more hardcoding, lets ask for input!
 def input_students
   puts "Please enter the names of the students"
-#  puts "Please also let me know where this student was born"
+  #  puts "Please also let me know where this student was born"
   puts "To finish, just hit return twice"
 
   #create an empty array
   students = []
   #get the first name
   name = gets.chomp
-
   #while the name is not empty, repeat this one
-
   while !name.empty? do
     #add the student hash to the array
     students << {name: name, cohort: :november}
@@ -26,40 +24,43 @@ def print_header
   puts "-" * 35
 end
 
-
-
-def starts_with_letter(students)
-puts "What letter are you lookign for?"
-letter = gets.chomp.upcase
-  #almost good, but the function should not iterate just if the letter is right
+def printing(students)
   students.each_with_index do |student, index|
-  if(student[:name].start_with?(letter))
-    puts "Name of students that starts with the letter: #{letter}"
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]})"
-  else
-    puts "That's all folks, no more names like this"
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
+def starts_with_letter(students)
+  puts "What letter are you lookign for?"
+  letter = gets.chomp.upcase
+  new_list = []
+  students.each_with_index do |student, index|
+    if(student[:name].start_with?(letter))
+      new_list << {name: student[:name], cohort: :november}
+    end
+  end
+  new_list.each_with_index do |student, index|
+    puts "Name of students that starts with the letter: #{letter}"
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]})"
+  end
 end
 
 
 def name_length(students)
+  puts "How long should a student's name be? (characters)"
+  name_length = gets.chomp
   students.each_with_index do |student, index|
-  if(student[:name].length < 12)
-    puts "Name of students who's name is shorter than 12 letters"
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-  else
-    puts "That's all folks, no more short names like this"
-  end
-end
-end
-
-
-def printing(students)
-  students.each_with_index do |student, index|
+    if(student[:name].length < 12)
+      puts "Name of students who's name is shorter than 12 letters"
       puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    else
+      puts "That's all folks, no more short names like this"
+    end
   end
 end
+
+
+
 
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
