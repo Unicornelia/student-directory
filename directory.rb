9239db01bff2f3ede11ede8d04a2993d03ae68b9
Kornelia <ez.kornelia@gmail.com>
2016-10-28 17:10:51 +0100

    Step 10 is done too from week 3 tasks

--- directory.rb
+++ directory.rb
@@ -1,7 +1,13 @@
+def space
+  puts ""
+end
+
+@students = []
+
 def input_students
   puts "Please enter the name of the students: \n"
   puts "To finish just hit return twice"
-  students = []
+  space
   name = gets.strip
   puts "What cohort does this student belong to?"
   cohort = gets.strip
@@ -17,8 +23,8 @@ def input_students
 
   while !name.empty? && !cohort.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: cohort.to_sym}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{@students.count} students"
     puts "Please enter the name of the next student: "
     name = gets.chomp
     puts "What cohort do they belong to? "
@@ -32,17 +38,18 @@ def input_students
       cohort = gets.chomp
     end
   end
-  students
+  @students
 end
 
 def print_header
   puts "The students of Villains Academy".center(50)
   puts "-------------------------".center(50)
+  space
 end
 
-def printing(students)
-  if students.length > 0
-    students.each_with_index do |student, index|
+def print_students_list
+  if @students.length > 0
+    @students.each_with_index do |student, index|
       puts "#{index + 1}. #{student[:name].capitalize} (#{student[:cohort].capitalize} cohort)".center(20)
     end
   else
@@ -50,59 +57,65 @@ def printing(students)
   end
 end
 
-def sort_by_cohort(students)
-  students.sort_by do |student|
+def sort_by_cohort
+  @students.sort_by do |student|
     student[:cohort]
     puts "The following students belong to the #{student[:cohort]} cohort: \n"
     puts "#{student[:name]}."
   end
 end
 
-def print_footer(names)
-  if names.count == 1
-    puts "Overall, we have #{names.count} great student".center(50)
-  elsif names.count == 0
+def print_footer
+  if @students.count == 1
+    space
+    puts "Overall, we have #{@students.count} great student".center(50)
+    space
+  elsif @students.count == 0
     puts "No students present, as I said, no list for you!"
     puts "Next time try to actually add something to the list. :) "
+    space
   else
-    puts "Overall, we have #{names.count} great students".center(50)
+    puts "Overall, we have #{@students.count} great students".center(50)
+    space
   end
 end
 
-def space
-  puts ""
+def print_menu
+  puts "Hello there, please choose what you wish to do: "
+  space
+  #1. orint the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" #there will be more items
 end
 
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    puts "Goodbye!"
+    exit #this will terminate our program
+  else
+    puts "I don't know what you meant, try again!"
+  end
+end
 
 def interactive_menu
-  students = []
-  puts "Hello there, please choose what you wish to do: "
   loop do
-    #1. orint the menu and asj the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" #there will be more items
-    #2. read the unput and save it into a variable
-    selection =  gets.chomp
-    #3. do what the user has asked
-    case selection
-    when "1"
-      space
-      students = input_students
-      space
-    when "2"
-      space
-      print_header
-      printing(students)
-      print_footer(students)
-      space
-    when "9"
-      puts "Goodbye!"
-      exit #this will terminate our program
-    else
-      puts "I don't know what you meant, try again!"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
+
+
 
 interactive_menu
