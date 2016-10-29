c178be84fd6c09f9ca1eb48dd8356afb1f74f44a
Kornelia <ez.kornelia@gmail.com>
2016-10-29 23:31:24 +0100

    Task 14/6 done, file closes itself

--- directory.rb
+++ directory.rb
@@ -132,24 +132,23 @@ end
 def save_students
   #open the file for writing
   save_file
-  file = File.open(@filename, "w")
-  #iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  File.open(@filename, "w") do |file|
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
 end
 
 def load_students
   load_file
-  file = File.open(@filename, "r")
-  file.readlines.each do |line|
-    @name, @cohort = line.chomp.split(",")
-    add_students
+  File.open(@filename, "r") do |file|
+    file.readlines.each do |line|
+      @name, @cohort = line.chomp.split(",")
+      add_students
+    end
   end
-  file.close
 end
 
 def try_load_students
